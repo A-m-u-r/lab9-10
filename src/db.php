488aeb75f828787f3127c465257015a8034804bb
daniil <112509251+A-m-u-r@users.noreply.github.com>
2026-05-23 13:41:45 +0500

    second version

--- src/db.php
+++ src/db.php
@@ -13,7 +13,6 @@ function db(): PDO
         mkdir($dataDir, 0775, true);
     }
     $dbPath = $dataDir . '/db.sqlite';
-
     $pdo = new PDO('sqlite:' . $dbPath, null, null, [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
